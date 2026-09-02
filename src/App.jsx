b243ae7c41284ae3bd93d9f8a9f8edd63a555fe0
Ekhaya-Mzansi <peacekgotso73@gmail.com>
2026-09-02 14:13:16 +0200

    Add App.jsx with MamKhizeBubble import and simple page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MamKhizeBubble from './components/MamKhizeBubble.jsx'
+
+export default function App(){
+  return (
+    <div style={{background:'#111',color:'white',minHeight:'100vh',padding:20}}>
+      <h1>EKHAYA LIVE</h1>
+      <MamKhizeBubble />
+    </div>
+  )
+}
